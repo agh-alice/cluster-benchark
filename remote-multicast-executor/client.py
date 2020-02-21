ce665eda8e5e27037b89d6b676ec5182894bc626
Rafał Mucha <6rafel6@gmail.com>
2020-02-21 19:12:11 +0100

    Add new tools to multicast execution

--- remote-multicast-executor/client.py
+++ remote-multicast-executor/client.py
@@ -1,8 +1,11 @@
+#!/bin/python3
 import socket
+import sys
+import functools
 
-UDP_IP = "224.0.0.1"
+UDP_IP = "224.0.70.71"
 UDP_PORT = 7070
-MESSAGE = b"make run"
+MESSAGE = functools.reduce(lambda x, y: x + " " + y, sys.argv[1:]).encode()
  
 print("UDP target IP:", UDP_IP)
 print("UDP target port:", UDP_PORT)
